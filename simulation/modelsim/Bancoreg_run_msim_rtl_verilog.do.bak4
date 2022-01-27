2d461de9a34a3ec9ad1175233a0d4d7e4f46c5e2
jhairssteven <71862429+jhairssteven@users.noreply.github.com>
2022-01-27 12:27:13 -0500

    display.v update to support 6 displays - easier registers' addresses and  data visualization

--- simulation/modelsim/Bancoreg_run_msim_rtl_verilog.do.bak4
+++ simulation/modelsim/Bancoreg_run_msim_rtl_verilog.do.bak4
@@ -5,11 +5,12 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -vlog01compat -work work +incdir+G:/Users/Administrador/Documents/UNAL\ Docs/2021\ -\ II/Electronica\ Digital\ I/Lab/lab04-2021-2-grupo05-2021-2/src {G:/Users/Administrador/Documents/UNAL Docs/2021 - II/Electronica Digital I/Lab/lab04-2021-2-grupo05-2021-2/src/BancoRegistro.v}
+vlog -vlog01compat -work work +incdir+G:/Users/Administrador/Documents/UNAL\ Docs/2021\ -\ II/Electronica\ Digital\ I/Lab/lab04-2021-2-grupo05-2021-2/src {G:/Users/Administrador/Documents/UNAL Docs/2021 - II/Electronica Digital I/Lab/lab04-2021-2-grupo05-2021-2/src/display.v}
+vlog -vlog01compat -work work +incdir+G:/Users/Administrador/Documents/UNAL\ Docs/2021\ -\ II/Electronica\ Digital\ I/Lab/lab04-2021-2-grupo05-2021-2/src {G:/Users/Administrador/Documents/UNAL Docs/2021 - II/Electronica Digital I/Lab/lab04-2021-2-grupo05-2021-2/src/BCDtoSSeg.v}
 
-vlog -vlog01compat -work work +incdir+G:/Users/Administrador/Documents/UNAL\ Docs/2021\ -\ II/Electronica\ Digital\ I/Lab/lab04-2021-2-grupo05-2021-2/src {G:/Users/Administrador/Documents/UNAL Docs/2021 - II/Electronica Digital I/Lab/lab04-2021-2-grupo05-2021-2/src/TestBench.v}
+vlog -vlog01compat -work work +incdir+G:/Users/Administrador/Documents/UNAL\ Docs/2021\ -\ II/Electronica\ Digital\ I/Lab/lab04-2021-2-grupo05-2021-2/src {G:/Users/Administrador/Documents/UNAL Docs/2021 - II/Electronica Digital I/Lab/lab04-2021-2-grupo05-2021-2/src/TB_display.v}
 
-vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cycloneive_ver -L rtl_work -L work -voptargs="+acc"  testbench
+vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cycloneive_ver -L rtl_work -L work -voptargs="+acc"  display
 
 add wave *
 view structure
--- src/display.v
+++ src/display.v
@@ -1,54 +1,44 @@
 `timescale 1ns / 1ps
-module display(
-    //input [15:0] num, // el programa era de 16 entradas (4 paquetes de BCD 4 bits)
-	 input [3:0] datoRA, // 4 bits salida registro A (1 display)
-	 input [3:0] datoRB, // 4 bits salida registro B (1 display)
-	 input [3:0] addRa, // 4 bits dirección registro A (1 display)
-	 input [3:0] addRb, // 4 bits dirección registro B (1 display)
-    input clk, //Reloj
-    output [0:6] sseg, // Salida 7 segmentos
-    output reg [3:0] an, // 4 anodos
-	 output led,
-	 input rst // reset
-	
+
+module display (
+	input [3:0] datoRA,   // 4 bits salida registro A (1 display)
+	input [3:0] datoRB,   // 4 bits salida registro B (1 display)
+	input [2:0] addRa, 	 // 4 bits dirección registro A (1 display)
+	input [2:0] addRb,    // 4 bits dirección registro B (1 display)
+	input [2:0] addrW,    // Dirección de escritura
+	input [3:0] datW,     // Dato a escribir
+   input clk,
+	input rst,
+   output [0:6] sseg,    // Salida 7 segmentos
+   output reg [5:0] an,  // Habilita 6 display
+	output led 			 	 // LED indicador de frecuencia	
     );
 
+	reg [3:0] bcd = 0;
+
+	BCDtoSSeg bcdtosseg(.BCD(bcd), .SSeg(sseg));
 
 
-reg [3:0]bcd=0;
- 
-BCDtoSSeg bcdtosseg(.BCD(bcd), .SSeg(sseg));
-
-reg [26:0] cfreq=0;
-wire enable;
-
-// Divisor de frecuecia
-
-assign enable = cfreq[16];
-assign led =enable;
-always @(posedge clk) begin
-  if(rst==1) begin
-		cfreq <= 0;
-	end else begin
-		cfreq <=cfreq+1;
+	reg [1:0] count = 0;
+	always @(posedge clk) begin
+			if(rst == 0) begin // cambiar a 1 para simulación en ModelSim
+				count <= 0;
+				an <= 6'b111111; 
+			end else begin 
+				if (count == 5) begin
+					count <= 0;
+				end else begin
+					count <= count + 1;
+				end
+				case (count) 
+					2'h0: begin bcd <= datoRA;   an <= 6'b111110; end 
+					2'h2: begin bcd <= addRa;    an <= 6'b111101; end 
+					2'h1: begin bcd <= datoRB;   an <= 6'b111011; end 
+					2'h3: begin bcd <= addRb;    an <= 6'b110111; end 
+					2'h4: begin bcd <= datW;     an <= 6'b101111; end 
+					2'h5: begin bcd <= addrW;    an <= 6'b011111; end 
+				endcase
+			end
 	end
-end
-
-reg [1:0] count =0;
-always @(posedge enable) begin
-		if(rst==1) begin
-			count<= 0;
-			an<=4'b1111; 
-		end else begin 
-			count<= count+1;
-			an<=4'b1101; 
-			case (count) 
-				2'h0: begin bcd <= datoRA;   an<=4'b1110; end 
-				2'h1: begin bcd <= datoRB;   an<=4'b1101; end 
-				2'h2: begin bcd <= addRa;    an<=4'b1011; end 
-				2'h3: begin bcd <= addRb;    an<=4'b0111; end 
-			endcase
-		end
-end
 
 endmodule
